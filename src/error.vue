c6a4a9e0576c4662acd1bcd47194b55311dddc46
vladustix <vladustix@gmail.com>
2024-12-19 13:20:15 +0700

    feat: error page

--- src/error.vue
+++ src/error.vue
@@ -1,17 +1,36 @@
 <script setup lang="ts">
 import type { NuxtError } from "#app";
 
-defineProps<{
+const props = defineProps<{
   error: NuxtError;
 }>();
+
+const message = computed(() => {
+  let text = props.error.message;
+  if (props.error.statusCode === 404) {
+    text = "Страница не найдена";
+  }
+  return text;
+});
 
 const handleError = () => clearError({ redirect: "/" });
 </script>
 
 <template>
-  <div>
-    <span>{{ error.statusCode }}</span>
-    <p>{{ error.message }}</p>
-    <button type="button" v-text="'Главная'" @click="handleError" />
-  </div>
+  <main class="w-full h-full min-h-screen grid">
+    <section class="m-auto w-full max-w-[400px] text-center">
+      <h1 v-text="error.statusCode" />
+      <p v-text="message" />
+      <button type="button" v-text="'Главная'" @click="handleError" />
+    </section>
+  </main>
 </template>
+
+<style scoped>
+h1 {
+  @apply font-medium text-6xl mb-3;
+}
+p {
+  @apply mb-3;
+}
+</style>
